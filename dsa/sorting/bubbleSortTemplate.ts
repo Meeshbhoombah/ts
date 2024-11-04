13aa70d6cd01472d84eea21c398f31a4c6bdd3a4
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-11-04 18:31:55 -0500

    Added reference for bubble sort

--- dsa/sorting/bubbleSortTemplate.ts
+++ dsa/sorting/bubbleSortTemplate.ts
@@ -5,6 +5,7 @@
  * Sorts a sequence of numbers
  *
  * Explainer: https://en.wikipedia.org/wiki/Bubble_sort
+ * Reference: https://naviava.hashnode.dev/bubble-sort-algorithm-in-typescript
  *
  */
 
